6bb779c78cebfa4441450fbe5a3f14ea58b9a320
noeLienard <noelienard2006@gmail.com>
2025-08-21 12:31:49 +0200

    lus

--- powerset.c
+++ powerset.c
@@ -1,70 +1,50 @@
-#include "stdio.h"
-#include "stdlib.h"
-#include "string.h"
-#include "unistd.h"
-#include <fcntl.h>
+#include <stdio.h>
+#include <stdlib.h>
 
-int	ft_atoi(char *str)
-{
-	int	i;
-	int	res;
-	int	sign;
-
-	i = 0;
-	res = 0;
-	sign = 1;
-	if (str[i] == '-')
-	{
-		sign = -1;
-		i++;
-	}
-	while (str[i] >= '0' && str[i] <= '9')
-	{
-		res = res * 10 + (str[i] - '0');
-		i++;
-	}
-	return (res * sign);
-}
-
-void	powerset(int size, int target, int i, int *set)
+void	find_subsets(int *set, int total, int size, int i, int target)
 {
 	int	sum;
-	int	n;
+	int	temp;
 
 	sum = 0;
-	n = i;
-	if (i == size)
-		return ;
+	temp = i;
+    if (i >= total)
+        return ;
 	for (int j = 0; j < size; j++)
 	{
-		if (n % 2 == 1)
+		if (temp % 2 == 1)
 			sum += set[j];
-        if (sum == target)
-            break ;
-		n /= 2;
+		temp /= 2;
 	}
 	if (sum == target)
 	{
+		temp = i;
 		for (int j = 0; j < size; j++)
 		{
-			if (n % 2 == 1)
+			if (temp % 2 == 1)
 				printf("%d ", set[j]);
-			n /= 2;
+			temp /= 2;
 		}
-        printf("\n");
+		printf("\n");
 	}
-	powerset(size, target, i + 1, set);
+    find_subsets(set, total, size, i + 1, target);
 }
 
 int	main(int argc, char **argv)
 {
 	if (argc < 3)
 		return (1);
+
 	int size = argc - 2;
+	int target = atoi(argv[1]);
 	int *set = malloc(sizeof(int) * size);
-	if (!set)
-		return (1);
-	for (int i = 0; argv[i + 2]; i++)
-		set[i] = ft_atoi(argv[i + 2]);
-	powerset(size, ft_atoi(argv[1]), 1, set);
+
+	for (int i = 0; i < size; i++)
+		set[i] = atoi(argv[i + 2]);
+	int total = 1;
+	for (int i = 0; i < size; i++)
+		total *= 2;
+	find_subsets(set, total, size, 0, target);
+	free(set);
+	return (0);
 }
